9bf28d2221a791c7a6f382b5c6b03379cd04797b
kguerrer <killian.guerrero@lkb.upmc.fr>
2024-08-13 13:09:42 +0200

    custom reward

--- utils.py
+++ utils.py
@@ -10,12 +10,18 @@ def hea(x, threshold=0.0):
 def inv_hea(x, threshold=0.0):
     return 1 if x < threshold else 0.0
 
+def porte(x, a, b):
+    return hea(x, a) + hea(-x, -b)
 
-A = np.linspace(-4*np.pi, 4*np.pi, 1000)
-Y = []
-for a in A:
-    y = (inv_hea(a, -2*np.pi) + hea(a, -2*np.pi) * gauss(a, -2*np.pi, 0.2*2*np.pi))
-    Y.append(y)
-plt.figure()
-plt.plot(A, Y)
-plt.show()
+def gaussp(x, mu, sigma):
+    return np.exp(-((x - mu) / sigma) ** 2) * porte(x, -sigma/2, sigma/2)
+
+
+# A = np.linspace(-4*np.pi, 4*np.pi, 1000)
+# Y = []
+# for a in A:
+#     y = porte(a, -2*np.pi, 2*np.pi)
+#     Y.append(y)
+# plt.figure()
+# plt.plot(A, Y)
+# plt.show()
